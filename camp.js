95cae0e663cddaa27d4a329a9a8411fb77106c2e
Milan Nakic <milan.nakic@gmail.com>
2020-04-04 19:21:06 +0200

    added just empty space in camp.js console log

--- camp.js
+++ camp.js
@@ -69,5 +69,5 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 //listener
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log("Our app is running on http://localhost:" + port);
+  console.log("Our app is running on http://localhost: " + port);
 });
