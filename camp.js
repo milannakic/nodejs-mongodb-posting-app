104b7a93456b62651b274ea11e9d37db5c2499c4
Milan Nakic <milan.nakic@gmail.com>
2020-04-05 14:19:13 +0200

    canhged connection method to avoid memory leaks

--- camp.js
+++ camp.js
@@ -8,21 +8,43 @@ const flash = require("connect-flash");
 const methodOverride = require("method-override");
 
 const User = require("./models/user");
-const seedDB = require("./seeds");
+//const seedDB = require("./seeds");
 
 //requring routes
 const campgroundsRoutes = require("./routes/campgrounds");
 const commentRoutes = require("./routes/comments");
 const indexRoutes = require("./routes/index");
 
-//Modify official guide
-mongoose.set("useFindAndModify", false);
+//Works but no connection close
+/* mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 mongoose.set("useUnifiedTopology", true);
 mongoose.connect(
   "mongodb+srv://milan:jehovah1985@cluster0-cxwkp.mongodb.net/yelp_camp?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
+  }
+); */
+
+const uri =
+  "mongodb+srv://milan:jehovah1985@cluster0-cxwkp.mongodb.net/yelp_camp?retryWrites=true&w=majority";
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  autoIndex: false, // Don't build indexes
+  poolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4, // Use IPv4, skip trying IPv6
+};
+mongoose.connect(uri, options).then(
+  () => {
+    /** ready to use. The `mongoose.connect()` promise resolves to mongoose instance. */
+  },
+  (err) => {
+    console.log(err);
   }
 );
 
