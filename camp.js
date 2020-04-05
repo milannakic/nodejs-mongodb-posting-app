cbb8bbfd1b611f2c9b4d7af493803cdba8d6ad1a
Milan Nakic <milan.nakic@gmail.com>
2020-04-05 15:17:41 +0200

    now camp.js has 1 local connection and 2 remote connection options

--- camp.js
+++ camp.js
@@ -8,13 +8,19 @@ const flash = require("connect-flash");
 const methodOverride = require("method-override");
 const User = require("./models/user");
 //const seedDB = require("./seeds");
-
-//requring routes
 const campgroundsRoutes = require("./routes/campgrounds");
 const commentRoutes = require("./routes/comments");
 const indexRoutes = require("./routes/index");
 
-//Works but no connection close
+//Local connection
+/* mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {
+  useNewUrlParser: true,
+}); */
+
+//REMOTE CONNECTION 1 - Works but no connection close
 /* mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 mongoose.set("useUnifiedTopology", true);
@@ -25,9 +31,8 @@ mongoose.connect(
   }
 ); */
 
-//Works as well but not connection close as well
-const uri =
-  "mongodb+srv://milan:jehovah1985@cluster0-cxwkp.mongodb.net/yelp_camp?retryWrites=true&w=majority";
+//REMOTE CONNECTION 2 - Works as well but not connection close as well
+const uriLocal = "mongodb://localhost:27017/yelp_camp";
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -39,7 +44,7 @@ const options = {
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
   family: 4, // Use IPv4, skip trying IPv6
 };
-mongoose.connect(uri, options).then(
+mongoose.connect(uriLocal, options).then(
   () => {},
   (err) => {
     console.log(err);
