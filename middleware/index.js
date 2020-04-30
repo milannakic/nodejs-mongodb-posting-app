094779482ada3da55b49b48830043380cb4e47be
Milan Nakic <milan.nakic@gmail.com>
2020-04-30 19:46:33 +0200

    Updated from bootstrap 3 to 4

--- middleware/index.js
+++ middleware/index.js
@@ -5,57 +5,57 @@ var Comment = require("../models/comment");
 var middlewareObj = {};
 
 middlewareObj.checkCampgroundOwnership = function (req, res, next) {
-    if (req.isAuthenticated()) {
-        Campground.findById(req.params.id, function (err, foundCampground) {
-            //must add || !foundCampground to cover cases where ID does match he pattern but it not valid/existing
-            if (err || !foundCampground) {
-                req.flash("error", "Campground not found");
-                res.redirect("back");
-            } else {
-                // does user own the campground?
-                if (foundCampground.author.id.equals(req.user._id)) {
-                    next();
-                } else {
-                    req.flash("error", "Permission denied, you do not own this");
-                    res.redirect("back");
-                }
-            }
-        });
-    } else {
-        req.flash("error", "Login first a-hole!");
+  if (req.isAuthenticated()) {
+    Campground.findById(req.params.id, function (err, foundCampground) {
+      //must add || !foundCampground to cover cases where ID does match he pattern but it not valid/existing
+      if (err || !foundCampground) {
+        req.flash("error", "Campground not found");
         res.redirect("back");
-    }
-}
+      } else {
+        // does user own the campground?
+        if (foundCampground.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          req.flash("error", "Permission denied, you do not own this sh1t");
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    req.flash("error", "Login first a-hole!");
+    res.redirect("back");
+  }
+};
 
 middlewareObj.checkCommentOwnership = function (req, res, next) {
-    if (req.isAuthenticated()) {
-        Comment.findById(req.params.comment_id, function (err, foundComment) {
-            if (err || !foundComment) {
-                req.flash("error", "Comment not found");
-                res.redirect("back");
-            } else {
-                // does user own the comment?
-                if (foundComment.author.id.equals(req.user._id)) {
-                    next();
-                } else {
-                    req.flash("error", "No, you didn't create this dude");
-                    res.redirect("back");
-                }
-            }
-        });
-    } else {
-        req.flash("error", "Login first idiot");
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+      if (err || !foundComment) {
+        req.flash("error", "Comment not found");
         res.redirect("back");
-    }
-}
+      } else {
+        // does user own the comment?
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          req.flash("error", "Noup can't do so, you didn't create this dude");
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    req.flash("error", "Login first ...idiot");
+    res.redirect("back");
+  }
+};
 
 middlewareObj.isLoggedIn = function (req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    //here we add a message to a middleware that can be called from other paces/routes
-    req.flash("error", "Yo a-hole, you must be logged in!");
-    res.redirect("/login");
-}
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  //here we add a message to a middleware that can be called from other paces/routes
+  req.flash("error", "Yo a-hole, you must be logged in!");
+  res.redirect("/login");
+};
 
 module.exports = middlewareObj;
--- routes/comments.js
+++ routes/comments.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 //add mergeParams: true to merge and fix issues with the id
 const router = express.Router({
-    mergeParams: true
+  mergeParams: true,
 });
 const Campground = require("../models/campground");
 const Comment = require("../models/comment");
@@ -11,96 +11,106 @@ const Comment = require("../models/comment");
 //we would have to specify "../middleware/filename.js"
 const middleware = require("../middleware");
 
-
 //with added isLoggedIn it will first check if user is logged in
 router.get("/new", middleware.isLoggedIn, function (req, res) {
-    Campground.findById(req.params.id, function (err, campground) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("comments/new", {
-                campground: campground
-            });
-        }
-    });
+  Campground.findById(req.params.id, function (err, campground) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("comments/new", {
+        campground: campground,
+      });
+    }
+  });
 });
 
 //with added isLoggedIn it will first check if user is logged in
 router.post("/", middleware.isLoggedIn, function (req, res) {
-    //lookup campground using ID
-    Campground.findById(req.params.id, function (err, campground) {
+  //lookup campground using ID
+  Campground.findById(req.params.id, function (err, campground) {
+    if (err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      Comment.create(req.body.comment, function (err, comment) {
         if (err) {
-            console.log(err);
-            res.redirect("/campgrounds");
+          req.flash("error", "Dude, smthng went wrong - maybe with DB");
+          console.log(err);
         } else {
-            Comment.create(req.body.comment, function (err, comment) {
-                if (err) {
-                    req.flash("error", "Dude, smthng went wrong - maybe with DB");
-                    console.log(err);
-                } else {
-                    //add username nd id to comment
-                    comment.author.id = req.user._id;
-                    comment.author.username = req.user.username;
-                    //save comment
-                    comment.save();
-                    //then we push that comment to other comments for that campground
-                    campground.comments.push(comment);
-                    campground.save();
-                    req.flash("success", "Great, you added a comment jerk");
-                    res.redirect('/campgrounds/' + campground._id);
-                }
-            });
+          //add username nd id to comment
+          comment.author.id = req.user._id;
+          comment.author.username = req.user.username;
+          //save comment
+          comment.save();
+          //then we push that comment to other comments for that campground
+          campground.comments.push(comment);
+          campground.save();
+          req.flash("success", "Great, you added a comment...jerk");
+          res.redirect("/campgrounds/" + campground._id);
         }
-    });
+      });
+    }
+  });
 });
 
 //comments EDIT FORM route
-router.get("/:comment_id/edit", middleware.checkCommentOwnership, function (req, res) {
-    //additional findById needed to make sure that a campground like that exists at all
-    //otherwise, a manual ID input (when it is a wrong id) would break the app
-    Campground.findById(req.params.id, function (err, foundCampground) {
-        if (err || !foundCampground) {
-            req.flash("error", "Campground not found");
-            return res.redirect("back");
-        }
-        Comment.findById(req.params.comment_id, function (err, foundComment) {
-            if (err) {
-                console.log(err);
-                res.redirect("back");
-            } else {
-                res.render("comments/edit", {
-                    campground_id: req.params.id,
-                    comment: foundComment
-                });
-            }
+router.get("/:comment_id/edit", middleware.checkCommentOwnership, function (
+  req,
+  res
+) {
+  //additional findById needed to make sure that a campground like that exists at all
+  //otherwise, a manual ID input (when it is a wrong id) would break the app
+  Campground.findById(req.params.id, function (err, foundCampground) {
+    if (err || !foundCampground) {
+      req.flash("error", "Campground not found");
+      return res.redirect("back");
+    }
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+      if (err) {
+        console.log(err);
+        res.redirect("back");
+      } else {
+        res.render("comments/edit", {
+          campground_id: req.params.id,
+          comment: foundComment,
         });
+      }
     });
+  });
 });
 
 //comments UPDATE / PUT route
-router.put("/:comment_id/", middleware.checkCommentOwnership, function (req, res) {
-    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, updatedComment) {
-        if (err) {
-            console.log(err);
-            res.redirect("back");
-        } else {
-            res.redirect("/campgrounds/" + req.params.id);
-        }
-    });
+router.put("/:comment_id/", middleware.checkCommentOwnership, function (
+  req,
+  res
+) {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (
+    err,
+    updatedComment
+  ) {
+    if (err) {
+      console.log(err);
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
 });
 
 //DELETE/DESTROY comments ROUTE
-router.delete("/:comment_id", middleware.checkCommentOwnership, function (req, res) {
-    Comment.findByIdAndDelete(req.params.comment_id, function (err) {
-        if (err) {
-            console.log(err);
-            res.redirect("back");
-        } else {
-            req.flash("success", "Aaaand now it's gone idiot");
-            res.redirect("/campgrounds/" + req.params.id);
-        }
-    });
+router.delete("/:comment_id", middleware.checkCommentOwnership, function (
+  req,
+  res
+) {
+  Comment.findByIdAndDelete(req.params.comment_id, function (err) {
+    if (err) {
+      console.log(err);
+      res.redirect("back");
+    } else {
+      req.flash("success", "Aaaand now it's gone idiot");
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
 });
-
 
 module.exports = router;
