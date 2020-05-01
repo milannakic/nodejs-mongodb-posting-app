dd0c38fafe1623943c1fb34aae3ed2a626aec67a
Milan Nakic <milan.nakic@gmail.com>
2020-05-01 18:30:50 +0200

    Added user profiles and new login page

--- models/user.js
+++ models/user.js
@@ -4,10 +4,11 @@ const passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = new mongoose.Schema({
   username: String,
   password: String,
-  isAdmin: {
-    type: Boolean,
-    default: false,
-  },
+  avatar: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  isAdmin: { type: Boolean, default: false },
 });
 
 UserSchema.plugin(passportLocalMongoose);
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 const User = require("../models/user");
+const Campground = require("../models/campground");
 
 //root route
 router.get("/", function (req, res) {
@@ -20,6 +21,10 @@ router.get("/register", function (req, res) {
 router.post("/register", function (req, res) {
   var newUser = new User({
     username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    avatar: req.body.avatar,
   });
   if (req.body.adminCode === process.env.ADMIN_CODE) {
     newUser.isAdmin = true;
@@ -61,6 +66,26 @@ router.get("/logout", function (req, res) {
   req.logOut();
   req.flash("success", "Logged you out cunt!");
   res.redirect("/campgrounds");
+});
+
+// USER PROFILE
+router.get("/users/:id", function (req, res) {
+  User.findById(req.params.id, function (err, foundUser) {
+    if (err) {
+      req.flash("error", "Something went wrong dude.");
+      return res.redirect("/");
+    }
+    Campground.find()
+      .where("author.id")
+      .equals(foundUser._id)
+      .exec(function (err, campgrounds) {
+        if (err) {
+          req.flash("error", "Sorry dude, no entries found by this user.");
+          return res.redirect("/");
+        }
+        res.render("users/show", { user: foundUser, campgrounds: campgrounds });
+      });
+  });
 });
 
 module.exports = router;
